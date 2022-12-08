4b73314c177125ee4bdb464af1653dccebb54fc6
Pablo Cavalcante <pablocavalcanteh@gmail.com>
2022-12-08 09:24:22 -0300

    Binary search tree - operations of search, pre order, in order and pos order.

--- tree/BinarySearchTree.py
+++ tree/BinarySearchTree.py
@@ -1,4 +1,7 @@
 
+from typing import Union
+
+
 class Node:
     
     def __init__(self, value):
@@ -38,3 +41,37 @@ class BinarySearchTree:
                         parent_node.right = node
                         self.links.append(str(parent_node.value) + '->' + str(node.value))
                         return
+    
+    def search(self, value) -> Union[Node, None]:
+        current_node = self.root
+        while current_node.value != value:
+            if value < current_node.value:
+                current_node = current_node.value
+            else:
+                current_node = current_node.right
+            if not current_node:
+                return None
+        return current_node
+    
+    # Root, lelf and right
+    def pre_order(self, node: Node) -> None:
+
+        if node:
+            print(node.value)
+            self.pre_order(node.left)
+            self.pre_order(node.right)
+    
+    # Left, root, right
+    def in_order(self, node: Node) -> None:
+        if node:
+            self.in_order(node.left)
+            print(node.value)
+            self.in_order(node.right)
+    
+    def pos_order(self, node: Node) -> None:
+        if node:
+            self.pos_order(node.left)
+            self.pos_order(node.right)
+            print(node.value)
+    
+    
