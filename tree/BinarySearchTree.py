50b04fc3324441172c52c1460a3d0cf6ceffd89d
Pablo Cavalcante <pablocavalcanteh@gmail.com>
2022-12-08 10:09:25 -0300

    Binary search tree - delete node.

--- tree/BinarySearchTree.py
+++ tree/BinarySearchTree.py
@@ -68,10 +68,119 @@ class BinarySearchTree:
             print(node.value)
             self.in_order(node.right)
     
-    def pos_order(self, node: Node) -> None:
+    # Left, right, root
+    def pos_order(self, node: Node) -> Union[None, bool]:
         if node:
             self.pos_order(node.left)
             self.pos_order(node.right)
             print(node.value)
-    
-    
+
+    def delete(self, value) -> None:
+        if not self.root:
+            print('The tree is empty...')
+            return None
+        
+        current_node = self.root
+        parent_node = self.root
+        _left = True
+
+        while current_node.value != value:
+            parent_node = current_node
+
+            if value < current_node.value:
+                _left = True
+                current_node = current_node.left
+            else:
+                _left = False
+                current_node = current_node.right
+            if not current_node:
+                return False
+        
+        if not current_node.left and not current_node.right:
+            if current_node == self.root:
+                self.root = None
+            elif _left:
+                self.links.remove(str(parent_node.value) + '->' + str(current_node.value))
+                parent_node.left = None
+            else:
+                self.links.remove(str(parent_node.value) + '->' + str(current_node.value))
+                parent_node.right = None
+        elif not current_node.right:
+            self.links.remove(str(parent_node.value) + '->' + str(current_node.value))
+            self.links.remove(str(current_node.value) + '->' + str(current_node.left.value))
+
+            if current_node == self.root:
+                self.root = current_node.left
+                self.links.append(str(self.root.value) + '->' + str(current_node.left.value))
+            
+            elif _left:
+                parent_node.left = current_node.left
+                self.links.append(str(parent_node.value) + '->' + str(current_node.left.value))
+            else:
+                parent_node.right = current_node.left
+                self.links.append(str(parent_node.value) + '->' + str(current_node.left.value))
+        
+        elif not current_node.left:
+
+            self.links.remove(str(parent_node.value) + '->' + str(current_node.value))
+            self.links.remove(str(current_node.value) + '->' + str(current_node.right.value))
+
+            if current_node == self.root:
+
+                self.links.append(str(self.root.value) + '->' + str(current_node.right.value))
+                self.root = current_node.right
+
+            elif _left:
+
+                self.links.append(str(parent_node.value) + '->' + str(current_node.right.value))
+                parent_node.left = current_node.right
+
+            else:
+
+                self.links.append(str(parent_node.value) + '->' + str(current_node.right.value))
+                parent_node.right = current_node.right
+        else:
+
+            next = self.get_next(current_node)
+
+            self.links.remove(str(parent_node.value) + '->' + str(current_node.value))
+            self.links.remove(str(current_node.right.value) + '->' + str(next.value))
+            self.links.remove(str(current_node.value) + '->' + str(current_node.left.value))
+            self.links.remove(str(current_node.value) + '->' + str(current_node.right.value))
+
+            if current_node == self.root:
+
+                self.links.append(str(self.root.value) + '->' + str(next.value))     
+                self.root = next
+
+            elif _left:
+
+                self.links.append(str(parent_node.value) + '->' + str(next.value))
+
+                parent_node.left = next
+
+            else:
+
+                self.links.append(str(parent_node.value) + '->' + str(next.value))
+                parent_node.right = next
+            
+            self.links.append(str(next.value) + '->' + str(current_node.left.value))
+            self.links.append(str(next.value) + '->' + str(current_node.right.value))
+            
+            next.left = current_node.left
+
+        return True
+        
+    def get_next(self, node: Node):
+        next_parent = node
+        next = node
+        current = node.right
+        while current != None:
+            next_parent = next
+            next = current
+            current = current.left
+        if next != node.right:
+            next_parent.left = next.right
+            next.right = node.right
+        return next
+
